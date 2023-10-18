c5aec5c64642da5830aefbc42604a437513963f9
Mihail <horunzhiy.m@yandex.ru>
2023-10-18 12:07:11 +0300

    Задача V_1

---
+++ Sem/1.1_DDT.py
@@ -0,0 +1,19 @@
+# Задание 1
+# С использованием фреймворка pytest написать тест операции checkText
+# SOAP API https://speller.yandex.net/services/spellservice?WSDL
+# Тест должен использовать DDT и проверять наличие определенного
+# верного слова в списке предложенных исправлений к определенному
+# неверному слову.
+# Слова должны быть заданы через фикстуры в conftest.py,
+# адрес wsdl должен быть вынесен в config.yaml.
+# Методы работы с SOAP должны быть вынесены в отдельную библиотеку.
+
+from zeep import Client, Settings
+import yaml
+with open("config.yaml", encoding="utf-8") as f:
+    data = yaml.safe_load(f)
+
+settings = Settings(strict=False)
+client = Client(wsdl=data["url"], settings=settings)
+
+print(client.service.checkText("малаток"))
